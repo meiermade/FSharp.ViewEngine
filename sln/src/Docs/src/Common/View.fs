6f24d9f958ccb65bf1d97a5dcc2ba5f163592472
Andy Meier <andy@meiermade.com>
2026-09-03 09:14:45 -0400

    MEIER-1022: Keep Docs ready during optional CDN loading

--- sln/src/Docs/src/Common/View.fs
+++ sln/src/Docs/src/Common/View.fs
@@ -104,7 +104,7 @@ module View =
             additionalHead =
                 [ link { _rel "icon"; _href "/favicon.svg"; _type "image/svg+xml" }
                   link { _rel "manifest"; _href "/site.webmanifest" }
-                  script { _src "https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1.0.22"; _type "module" } ] }
+                  script { _src "https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1.0.22"; _type "module"; _async true } ] }
 
     let private site (sections:NavSection list) search : DocsSite<string> =
         { name = "FSharp.ViewEngine"
